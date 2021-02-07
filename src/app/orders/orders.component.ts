2c3b7dd473fc16c19878259e4dde27c7a650da88
Ziad <ziad32ahmed111@gmail.com>
2021-02-07 22:58:21 +0200

    add open layers map

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -3,7 +3,11 @@ import { Order } from '../order';
 import { OrderService } from '../order.service';
 import { Subscription } from 'rxjs';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Map, layer, source, View, proj } from 'openlayers';
 
+
+const INITIAL_OPACITY = 1;
+const DIMMED_OPACITY = 0.3;
 
 @Component({
     selector: 'app-orders',
@@ -18,6 +22,8 @@ export class OrdersComponent implements OnInit, OnDestroy {
         request: new FormControl(null, Validators.required)
     });
 
+    private map: Map;
+
     lat = 51.678418;
     lng = 7.809007;
 
@@ -29,6 +35,17 @@ export class OrdersComponent implements OnInit, OnDestroy {
 
         this.subscriptions.push(httpSubscription);
         this.subscriptions.push(socketSubscription);
+
+        this.map = new Map({
+            layers: [
+              new layer.Tile({ source: new source.OSM(), opacity: INITIAL_OPACITY }),
+            ],
+            target: document.getElementById('map'),
+            view: new View({
+              center: proj.transform([-0.12755, 51.507222], 'EPSG:4326', 'EPSG:3857'),
+              zoom: 3
+            })
+          });
     }
 
     ngOnDestroy() {
